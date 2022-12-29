75428294afedddc84d5f630c797a8d66fd9dbc8c
sikarwar2010 <sikarwar2010@gmail.com>
2022-12-29 14:24:46 +0530

    initial app update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Tarun Sikarwar!';
+    return 'Hello World! Tarun Sikarwar! we are start new project in MSME Industry!';
   }
 }
